9c3daafc16a9e726302b823bfb1f35c4cb1a6a5d
Crispeuw360 <igornikolaidis@gmail.com>
2025-12-16 10:37:22 +0100

    Primeras anotaciones con hibernate,Profile,User,Admin

--- PRG/RetoFinal/src/model/User.java
+++ PRG/RetoFinal/src/model/User.java
@@ -1,12 +1,19 @@
 package model;
+import javax.persistence.*;
 
 /**
  * Represents a standard user in the system.
  * Extends Profile and adds gender and card number attributes.
  */
+@Entity
+@Table(name="USER_")
+@PrimaryKeyJoinColumn(name="username", referencedColumnName="username")
 public class User extends Profile {
+    @Column(name="gender",length=50)
     private String gender;
+    @Column(name="card_number", length=24)
     private String cardNumber;
+    
 
     public User(String gender, String cardNumber, String username, String password, String email, int userCode, String name, String telephone, String surname) {
         super(username, password, email, userCode, name, telephone, surname);
